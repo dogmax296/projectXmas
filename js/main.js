c9c3ebf63e686c437f32a8ae827a165ede6bfabe
dogmax296 <ivanov1maxim@gmail.com>
2015-12-18 18:50:18 +0200

    Textarea progress bar finish

--- js/main.js
+++ js/main.js
@@ -1,19 +1,39 @@
 // Progress for textarea
 
+
+
 jQuery(document).ready(function() {
+
+    // TOP
+
+    $('.form__textarea--top').keyup(function() {
+        var box = $(this).val();
+        var main = box.length * 100;
+        var value = parseInt((main / 132),10);
+        var count = 0 + value;
+
+        
+        if(box.length >= 0){
+
+            $('.form__progress-bar--top').css('width', count + '%');
+            $('.form__current--top').text(count + '%');   
+        }
+        return false;
+    });
+
+    // BOTTOM
     
     $('.form__textarea--bottom').keyup(function() {
         var box = $(this).val();
         var main = box.length * 100;
-        var value = (main / 130);
-        var count = 0 + box.length;
-        var reverse_count = 130 - box.length;
+        var value = parseInt((main / 132),10);
+        var count = 0 + value;
+        var count_lenght = box.length;
         
         if(box.length >= 0){
 
-            $('.progress-bar').css('width', count + '%');
-            $('.current-value').text(count + '%');
-            $('.count').text(reverse_count);    
+            $('.form__progress-bar--bottom').css('width', count + '%');
+            $('.form__count-left--bottom').text(count_lenght);    
         }
         return false;
     });
@@ -29,5 +49,18 @@ $(document).ready(function() {
     $(".date-now__month").text(now.format("MMM"));
     $(".date-now__day").text(now.format("D"));
     $(".date-now__year").text(now.format("YYYY"));
+
+});
+
+// DAYS till New date-now__year
+
+$(document).ready(function() {
+
+    var todayDate = moment().date();
+    var newYearDate = 31;
+    var daysLeft = newYearDate - todayDate;
+
+    $(".popup__newyear-left").text(daysLeft);
+    
 
 });
